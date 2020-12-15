01b0d6b9122ccae52b31658ddf9244b82ef62126
Tajmirul <tasmirolislam@gmail.com>
2020-12-15 09:47:23 +0600

    Updated

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 $(document).ready(function () {
 
-//     /* magnificPopup img view */
-//     $('.popup-image').magnificPopup({
-//         type: 'image',
-//         gallery: {
-//             enabled: true
-//         }
-//     });
+	// magnificPopup img view 
+    $('.popup-image').magnificPopup({
+        type: 'image',
+    });
 
 //     /* magnificPopup video view */
 //     $('.popup-video').magnificPopup({
@@ -30,6 +27,48 @@ $(document).ready(function () {
 	    // autoplay: true,
 	    slidesToShow: 1,
 	    slidesToScroll: 1,
+	});
+
+	$('.slider-6').slick({
+		slidesToShow: 6,
+		slidesToScroll: 2,
+        prevArrow: '<i class="arrows far fa-chevron-left left"></i>',
+        nextArrow: '<i class="arrows far fa-chevron-right right"></i>',
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					autoplay: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					autoplay: true
+				}
+			}
+		]
+	});
+
+	$('.slider-4').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+        prevArrow: '<i class="arrows far fa-chevron-left left"></i>',
+        nextArrow: '<i class="arrows far fa-chevron-right right"></i>',
+        infinite: false,
+		responsive: [
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3,
+					autoplay: true
+				}
+			}
+		]
 	});
 
 //     // my code ----------------------------------------------------------------------
@@ -59,35 +98,39 @@ $(document).ready(function () {
 //     });
 
 //     // menu bar open -------------------------
-//     $('.navigations .menu-icon').click(function () {
-//         $('.mobile-navigations').css({
-//             'width': '100%',
-//             'padding': '3rem 5rem'
-//         });
-//         // $('.mobile-navigations').toggleClass('w-100 py-5 pl-50 pr-50')
-//     });
-//     // menu bar close -------------------------
-//     $('.mobile-navigations li.close-menu').click(function () {
-//         $('.mobile-navigations').css({
-//             'width': '0',
-//             'padding': '0'
-//         });
-//     });
-//     // dropdown toggle ------------------
-//     $('.mobile-navigations li.has-dropdown > i').click(function () {
-//         $(this).parent().find('>ul').slideToggle()
-//     });
+    $('.open-mobile-menu ').click(function () {
+        $('.mobile-menu').fadeIn(200);
+    });
+    // menu bar close -------------------------
+    $('.close-mobile-menu').click(function () {
+        $('.mobile-menu').fadeOut(200);
+    });
 
 	let sliderInput = $('.slider-input');
 	let sliderOutput = $('.slider-output .number');
 
-	sliderOutput.text(sliderInput[0].value);
+	if (sliderInput[0] && sliderOutput) {
+		sliderOutput.text(sliderInput[0].value);
 
-	sliderInput.on('change', function() {
-		console.log($(this).value)
+		sliderInput.on('change', function() {
+			console.log($(this).value)
 
-		sliderOutput.text($($(this)[0]).val());
-	});
+			sliderOutput.text($($(this)[0]).val());
+		});
+	};
 
+	const thumbs = $('.product-thumb');
+
+	$.each(thumbs, (index, item) => {
+
+		$(item).click(function(event) {
+			event.preventDefault();
+
+			console.log($(this)[0]);
+
+			$('.product-img img').attr('src', $(item).children('img').attr('src'));
+			$('.product-img[data-popup=true] a').attr('href', $('.product-img[data-popup=true] img').attr('src'));
+		});
+	});	
 
 });
